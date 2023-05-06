3fd7eefe887407e4bcd08952d6532bfc39990837
SAAD HOSSAIN <132562957+hossainsaad@users.noreply.github.com>
2023-05-06 18:10:36 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main{
+//     public static void main (String[] args) {
+//       for (int i=1;i<=2;i++){
+          
+      
+//       for (int j=1;j<=2;j++)
+//       {
+//       System.out.print("!");
+//       }
+//       System.out.println();
+//       }
+//     }
+// } 
+
+public static void main (String[] args) {
+//     int n = 5;
+//     for (int i=1;i<=n;i++)
+//     {
+//         for (int j=1;j<=i;j++)
+//         {
+//             System.out.printf("%2d", i*j);
+//         }
+//         System.out.println(" ");
+//         }
+//     }
+// }
+
